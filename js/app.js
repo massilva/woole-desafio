de1c5151c4b997ff8b4f35ec0a418021d2f02882
Marcos Antônio de Souza Silva <chuckymass@gmail.com>
2016-04-30 10:37:55 -0300

    Cálcula o vetor de menor distancia dos pontos carregados pelo KML

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
             tileLayer,
             layers,
             search,
-            adjacencyMatrix = {};
+            distanceMatrix = {},
+            leastDistanceList = {};
 
         //inicializa o mapa com a visão de salvador e no zoom max
         map = L.map('mapa').setView([-13.0015785, -38.507568], 18);
@@ -21,12 +22,27 @@
         layers = new L.KML("data/pontos.kml", {async: true});
         layers.on("loaded", function (e) {
             map.fitBounds(e.target.getBounds());
+
+            // Atualiza a matrix de distancia
             layers.eachLayer(function (source) {
-                updateAdjancyMatrix(source);
+                updateDistanceMatrix(source);
                 source.on("click", function () {
                     this.closePopup();
                 });
             });
+
+            // Atualiza a lista de menor distancia
+            layers.eachLayer(function (source) {
+                leastDistanceList[source._leaflet_id] = source._leaflet_id;
+                layers.eachLayer(function (target) {
+                    if (source !== target) {
+                        if(distanceMatrix[source._leaflet_id][leastDistanceList[source._leaflet_id]] > distanceMatrix[source._leaflet_id][target._leaflet_id]) {
+                            leastDistanceList[source._leaflet_id] = target._leaflet_id;
+                        }
+                    }
+                });
+            });
+
         });
         map.addLayer(layers);
 
@@ -44,7 +60,7 @@
                 jsonpParam: 'json_callback',
                 propertyName: 'display_name',
                 propertyLoc: ['lat', 'lon'],
-                circleLocation: true,
+                circleLocation: false,
                 markerLocation: false,
                 autoType: false,
                 autoCollapse: true,
@@ -58,10 +74,10 @@
 
                 if (!pos) {
                     search.layer.eachLayer(function (layer) {
-                        for  (id in adjacencyMatrix[layer._leaflet_id]) {
-                            delete adjacencyMatrix[layer._leaflet_id][id];
+                        for  (id in distanceMatrix[layer._leaflet_id]) {
+                            delete distanceMatrix[layer._leaflet_id][id];
                         }
-                        delete adjacencyMatrix[layer._leaflet_id];
+                        delete distanceMatrix[layer._leaflet_id];
                         search.layer.removeLayer(layer);
                     });
                 }
@@ -72,21 +88,20 @@
                 $("#searchtext10").attr('placeholder', search.placeholders[pos ? 0 : 1]);
 
                 layers.addLayer(marcador);
-                updateAdjancyMatrix(marcador);
+                updateDistanceMatrix(marcador);
 
             });
 
         map.addControl(search.control);
         $("#searchtext10").attr('placeholder', search.placeholders[0]);
 
-        //Atualiza matrix de adjacencia
-        function updateAdjancyMatrix(marcador) {
+        function updateDistanceMatrix(marcador) {
             var id, layer;
-            adjacencyMatrix[marcador._leaflet_id] = {};
-            for (id in adjacencyMatrix) {
+            distanceMatrix[marcador._leaflet_id] = {};
+            for (id in distanceMatrix) {
                 layer = layers.getLayer(id);
-                adjacencyMatrix[id][marcador._leaflet_id] = layer === marcador ? Infinity : layer.getLatLng().distanceTo(marcador.getLatLng());
-                adjacencyMatrix[marcador._leaflet_id][id] = adjacencyMatrix[id][marcador._leaflet_id];
+                distanceMatrix[id][marcador._leaflet_id] = layer === marcador ? Infinity : layer.getLatLng().distanceTo(marcador.getLatLng());
+                distanceMatrix[marcador._leaflet_id][id] = distanceMatrix[id][marcador._leaflet_id];
             }
         }
 
